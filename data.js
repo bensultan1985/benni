821cbef5a0c78eed99527d2f09ff964b17c5cf71
bens <bensultan1985@gmail.com>
2021-04-25 20:27:03 -0700

    v1 ready for launch

---
+++ data.js
@@ -0,0 +1,107 @@
+_phraseList = {
+    "seeking" : {
+        tags: ['why', 'seeking', 'job', 'want', 'here', 'company'],
+        responses: ["I want to join an exciting dev team.", "I want a job either in California or remote.", "I want to find the right opportunit for me."]
+    },
+
+    "pleasantries" : {
+        tags: ['how', 'today', 'doing', 'are'],
+        responses: ["I'm great, thanks", "I am lucky - my 2021 has been pretty good. I hope yours has been too. What else would you like to ask me?"]
+    },
+
+    "experience" : {
+        tags: ['experience', 'experienced', 'do', 'know', 'have', 'skills', 'skill', 'use',"sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic"],
+        responses: ["sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic"]
+    },
+
+    "salary" : {
+        tags: ['salary', 'annual', 'benefits', 'retirement', 'expected', 'range'],
+        responses: ["It depends on the benefits and location, but the lower end of my salary expectation is 75k. This is all negotiable."]
+    },
+
+    "onsite" : {
+        tags: ['vaccinated', 'vaccine', 'on-site', 'in-person', 'in', 'person', 'location', 'remote'],
+        reponses: ["Yes, I have been vaccinated, and I can work on site if needed."]
+    },
+
+    "relocate" : {
+        tags: ['relocate', 'relocation', 'expense', 'travel'],
+        responses: ["Yes, I am ready and willing to relocate for the right position. And, I am ready for business travel."]
+    },
+
+    "located" : {
+        tags: ['where', 'city', 'state', 'located', 'from', 'live', 'currently'],
+        responses: ['I am currently located in Culver City, California (in Los Angeles County).']
+    },
+
+    "start" : {
+        tags: ['start', 'job', 'date', 'contract', 'begin', 'work', 'soon', 'ready'],
+        responses: ["I can start tomorrow."]
+    },
+
+    "hobbies" : {
+        tags: ['what', 'have', 'hobbies', 'free', 'time', 'interests', 'activities', 'talents', 'talent', 'special', 'other', 'personal', 'fun'],
+        responses: ["I enjoy writing and recording music, and I am a card-carrying member of the International Jugglers' Association"]
+    },
+
+    "strengths" : {
+        tags: ['what', 'strengths', 'strength', 'talent', 'skill', 'confident', 'abilities'],
+        responses: ["I enjoy problem solving, i am a clear communicator, and I am a javscript expert."]
+    },
+
+    "weaknesses" : {
+        tags: ['what', 'weakness', 'weaknesses', 'shortcoming', 'shortcomings'],responses: ["Chocolate and peanut butter."]
+    },
+
+    "questions" : {
+        tags: ['questions', 'me', 'for', 'ask'],
+        responses: ["No, but since you stayed this long to chat, I think you should reach out to the real Ben. Something tells me that he'll be very interested in speaking with you."]
+    },
+
+    "education" : {
+        tags: ['degree', 'education', 'highest', 'school', 'graduate', 'science', 'school', 'college', 'where'],
+        responses: ["My highest degree is a master's in education from Sarah Lawrence College. I also have a web development certificate from the CodeCademy Mentorship Program. Ben would love to give you more details about his unique mentorship-based training in a phone interview."]
+    },
+
+    "stack" : {
+        tags: ['full-stack', 'full', 'stack', 'front', 'back', 'framework', 'frameworks'],
+        responses: ["I am a full stack developer. For my personal projects, I normally use something close to the MERN stack, with a couple of modifications, such as replacing MongoDB with Firebase, my prefered NoSQL database."]
+    },
+
+    "years" : {
+        tags: ['years', 'experience', 'professional', 'do', 'programming', 'job', 'work', 'working', 'many'],
+        responses: ["I have been developing personal projects for two years. I have 8 months' professional experience as a software engineer."]
+    },
+    "greeting" : {
+        tags: ['hello', 'hey', 'hi'],
+        responses: ["Hi, nice to meet you. I love being interviewed. Ask away!", "Hello yourself! I'm excited that you'll be interviewing me.", "Hey! I had my coffee and eggs this morning. I'm ready for this interview."]
+    },
+    "too" : {
+        tags: ["me, too"],
+        responses: ["Thanks for sharing. What else can I answer for you?"]
+    },
+    "contact" : {
+        tags: ["contact, info, email, phone, call"],
+        responses: ["It sounds like I really nailed this virtual interview. The real Ben's email is bensultan1985@gmail.com. Ben is going to be excited to hear from you! And I might get a bonus for my good work. Right now, I live on a Heroku server, but I'm saving up for my very own domain.", "The real Ben's email address is bensultan1985@gmail.com. He is going to be so happy to hear from you."]
+    },
+    "schedule" : {
+        tags: ["appointment", "schedule", "interview", "real"],
+        responses: ["Right now, I am a limited bot that can't schedule interviews, so you will have to email Ben directly at bensultan1985@gmail.com"]
+    },
+    "interpersonal" : {
+        tags: ["style", "communication", "colleagues", "along", "conflict", "issues", "issue", "approach", "other"],
+        responses: ["I think you are asking me about how I communicate with people at work. I am professional and communicate clearly and effectively. If interpersonal conflicts arise, I will address the problem directly and immediately so that we can return to a friendly, productive environment."]
+    },
+    "solving": {
+        tags: ["problem", "problems", "challenge", "approach", "issue", "issues", "bugs", "bug", "debugging", "deadlines", "deadline", "requirements"],
+        responses: ["I design and plan from the top down before beginning to code, which helps me avoid major issues later. However, bugs and challenges arise often in programming. I enjoy solving problems."]
+    },
+    "company" : {
+        tags: ["what", "kind", "type", "company", "dev", "team", "development", "products", "software", "like", "want"],
+        responses: ["I think you are asking what type of company I would like to work for. The answer is, I would like to ideally work for a tech company that makes intuitive, leading-edge tech products (like Apple and Amazon). If your company is not primarily a tech company, that's okay too - other sectors that greatly interest me are fitness, education, and entertainment!"]
+    },
+    "position" : {
+        tags : ["developer", "programmer", "job", "position", "role", "level", "what", "looking", "want", "engineer", "seeking"],
+        responses: ["I am seeking a position as a Software Engineer."]
+    }
+}
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var introMsg = `You are a busy person, and Ben is too. That's why he created me, Benni, a bot that is able to answer job interview questions! Ask a few questions. If my answers are to your liking, consider giving the real Ben an interview.`
 var mainContainer = document.getElementById('main-container');
+var _currentQuery = '';
+var _intel = {};
+var _notSure = ["I'm a mere bot, and I did not understand the question. Can you ask it another way?", "If you ask that another way, perhaps I can understand it.", "I'm a little confused about the question. Try to ask one more time in a slightly different way, please."];
+var _notSureElevated = ["Sorry, but I still don't understand. Ask me a new question.", "I'm still confused. This would be a great question to ask Ben directly.", "Ah, the life of a bot. I have failed my only task. I still don't understand your question. Ask me something else."];
+var _notSureLevel = _notSure;
+var _postCount = 0;
+var _respCount = 0;
 
 function onLoad(){
     var introP = genElement('p', 'intro-text', '', introMsg)
@@ -12,6 +19,7 @@ function initBenniSys() {
     var sendForm = createSendForm();
     var textOut = createAnsDiv();
     appendChildren(mainContainer, [textOut, sendForm])
+    resizeBox();
 }
 
 function createSendForm() {
@@ -19,6 +27,19 @@ function createSendForm() {
     var qInput = createQInput();
     var qSend = createQSend();
     appendChildren(form, [qInput, qSend])
+    qSend.addEventListener('click', (event) => {
+      console.log(event)
+      event.preventDefault();
+      _currentQuery = qInput.value;
+      getQueryArray(_currentQuery);
+      qInput.value = '';
+    })
+    qSend.addEventListener('submit', (event) => {
+      event.preventDefault();
+      _currentQuery = qInput.value;
+      getQueryArray(_currentQuery);
+      qInput.value = '';
+    })
     return form;
 }
 
@@ -66,9 +87,121 @@ function genElement(type, id, className, innerHTML, value, nameAttr, otherAttr) 
   };
 
   function resizeBox() {
-      
+     var mainHeight = $('#main-container').height();
+     if (document.getElementById('text-out')) {
+     $('#text-out').height(mainHeight-100)
+     }
   }
 
   $(window).resize(function() {
     resizeBox();
   });
+
+  function getQueryArray(queryStr) {
+    if (queryStr == '') return false;
+    getTextMsg(queryStr, 'post');
+    var array = queryStr.toLowerCase().split(' ');
+    for (let i = 0; i < array.length; i++) {
+      var word = array[i];
+      var str = '';
+      if (word[word.length-1] == '.' || word[word.length-1] == '?' || word[word.length-1] == '!') {
+        str = word.slice(0, word.length - 1);
+        array[i] = str;
+        console.log(str)
+      }
+    }
+    console.log(array)
+    buildIntel(array)
+  }
+
+  function buildIntel(array) {
+    _intel = {};
+    var expArr = [];
+    array.forEach(word => {
+      console.log(word)
+      for (phrase in _phraseList) {
+        if (!_intel[phrase]) {
+          _intel[phrase] = {
+            foundTags: [],
+            count: 0
+          };
+      }
+        _phraseList[phrase].tags.forEach(tag => {
+          if (word == tag) {
+            _intel[phrase].foundTags.push(word);
+            _intel[phrase].count++;
+            if (phrase == 'experience') {
+              _phraseList[phrase].responses.forEach(resp => {
+                if (word == resp) {
+                  expArr.push(word)
+                }
+              })
+            }
+          }
+        })
+      }
+    })
+    // console.log(_intel)
+    var sorted = Object.keys(_intel).sort(function(a, b){
+      console.log(_intel[a].count,_intel[b].count)
+        return _intel[b].count - _intel[a].count;
+    })
+    buildResponse(sorted, expArr);
+  }
+
+  function buildResponse(arr, expArr) {
+    var sentence = '';
+    var targetPhrase = _intel[arr[0]];
+    if (targetPhrase.count >= 2) {
+      _notSureLevel = _notSure;
+      if (arr[0] != 'experience') {
+        sentence = _phraseList[arr[0]].responses[0];
+      } else {
+        for (let i = 0; i < expArr.length; i++) {
+          if (expArr.length > 2 && i < expArr.length-2 && i > 0) {
+            sentence += ', ' + expArr[i] + ''
+            continue;
+          }
+          if (i == 0) sentence += ' ' + expArr[i] + ' ';
+          if (i > 0 && i == expArr.length-1) sentence += 'and ' + expArr[i] + '.'
+        }
+        sentence != ''? sentence = 'Yes, I know ' + sentence : sentence = "I don't think I've studied that skill much yet, but I am a quick study."
+      }
+    } else {
+      if (targetPhrase.count == 1 && arr[0] == 'greeting') {
+        sentence = _phraseList[arr[0]].responses[0];
+      } else {
+        sentence = _notSureLevel[Math.floor(Math.random() * 3)];
+        _notSureLevel != _notSureElevated? _notSureLevel = _notSureElevated : _notSureLevel = _notSure;
+      }
+    }
+    getTextMsg(sentence, 'response');
+  }
+
+  function getTextMsg(sentence, type) {
+    count = 0;
+    var msg = '...'
+    if (type != 'response') msg = sentence;
+    type == 'response'? count = _respCount : count = _postCount;
+    var container = genElement('div', 'text-' + type + count, 'text-container-' + type, msg)
+    type == 'response'? _respCount++ : _postCount++;
+    var textOut = document.getElementById('text-out');
+    console.log(type)
+    if (type == 'response') {
+      setTimeout(function(){
+        textOut.appendChild(container);
+        scrollDown();
+      }, 500)
+      setTimeout(function(){
+        container.innerHTML = sentence;
+        scrollDown();
+      }, 2000)
+    } else {
+      textOut.appendChild(container);
+    }
+    scrollDown();
+  }
+
+  function scrollDown() {
+    document.getElementById("text-out").scrollTop = document.getElementById('text-out').scrollHeight;
+  }
