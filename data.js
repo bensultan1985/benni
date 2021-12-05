498cbc62a58fd3ea619c219796de6080bc292c5c
bensultan1985 <bensultan1985@gmail.com>
2021-12-05 13:20:54 -0800

    updated chat data

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 _phraseList = {
     "seeking" : {
         tags: ['why', 'seeking', 'job', 'want', 'here', 'company'],
-        responses: ["I want to join an exciting dev team.", "I want to find the right opportunit for me."]
+        responses: ["I want to join an exciting dev team.", "I want to find the right job opportunity."]
     },
 
     "pleasantries" : {
         tags: ['how', 'today', 'doing', 'are'],
-        responses: ["I'm great, thanks", "I am lucky - my 2021 has been pretty good. I hope yours has been too. What else would you like to ask me?"]
+        responses: ["I'm great, thanks.", "I am lucky - my 2022 has been pretty good. I hope yours has been too. What else would you like to ask me?"]
     },
 
     "experience" : {
-        tags: ['experience', 'experienced', 'how', 'do', 'know', 'have', 'skills', 'skill', 'use',"sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic"],
-        responses: ["sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic"]
+        tags: ['experience', 'experienced', 'how', 'do', 'know', 'have', 'skills', 'skill', 'use',"sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic", "typescript", "gatsby", "docker", "sass", "react native", "responsive design", "design", "wireframing", "libraries", "aws", "gcp"],
+        responses: ["sql", "nosql", "react", "react.js", "javascript", "js", "node", "node.js", "express.js", "express", "aws", "mysql", "mongodb", "jquery", "testing", "jest", "data", "structures", "algorithms", "bootstrap", "apis", "api", "restfulapis", "git", "github", "html", "html5", "css", "css3", "web development", "mern", "logic", "typescript", "gatsby", "docker", "sass", "react native", "responsive design", "design", "wireframing", "libraries", "aws", "gcp"]
     },
 
     "salary" : {
         tags: ['salary', 'annual', 'benefits', 'retirement', 'expected', 'range', 'what', 'requirements', 'expectation'],
-        responses: ["It depends on the benefits and location, but the lower end of my salary expectation is 75k. This is all negotiable."]
+        responses: ["My desired salary is 100k with health and retirement benefits."]
     },
 
     "onsite" : {
-        tags: ['vaccinated', 'vaccine', 'on-site', 'in-person', 'in', 'person', 'location', 'remote'],
-        reponses: ["Yes, I have been vaccinated, and I can work on site if needed."]
+        tags: ['vaccinated', 'vaccine', 'on-site', 'in-person', 'in', 'person', 'location', 'remote', "hybrid"],
+        reponses: ["Yes, I have been vaccinated, and I am open to all work styles, including remote, on-site, and hybrid."]
     },
 
     "relocate" : {
         tags: ['relocate', 'relocation', 'expense', 'travel', 'willing', 'can', 'would'],
-        responses: ["Yes, I am ready and willing to relocate for the right position. And, I am ready for business travel."]
+        responses: ["Yes, I am willing to relocate for the right position, and I can travel for work."]
     },
 
     "located" : {
@@ -36,7 +36,7 @@ _phraseList = {
 
     "start" : {
         tags: ['start', 'job', 'date', 'contract', 'begin', 'work', 'soon', 'ready', 'when'],
-        responses: ["I can start tomorrow."]
+        responses: ["My current employer needs one month notice."]
     },
 
     "remote" : {
@@ -46,26 +46,26 @@ _phraseList = {
 
     "skillset" : {
         tags: ['what', 'skills', 'have', 'strongest'],
-        responses: ["I program with Javascript every day. I also use other full-stack languages and frameworks like Node.js, Express.js, React, SQL, and NoSQL. Ask me about specific skills, and I can tell you if I have experience with them."]
+        responses: ["I program with Javascript every day. I also use other full-stack languages and frameworks like Node.js, Express.js, React, React Native, SQL, and NoSQL. Ask me about specific skills, and I can tell you if I have experience with them."]
     },
     "planning" : {
         tags: ['how', 'what', 'approach', 'design', 'project', 'team', 'sketch', 'figma', 'wireframe', 'organize'],
-        responses: ["I think you are asking about project planning and design. I approach projects with attention to the big picture and to details. That is why before I begin, I may create spreadsheets, draw wireframes, and discuss best practices with my teammates. I would keep track of my project by updating a design document."]
+        responses: ["I think you are asking about project design. My approach to design is top-down, beginning with the big picture, and then working out the details. I write clear documentation for every project."]
     },
 
     "resume" : {
         tags: ['resume', 'have', 'do'],
-        responses: ["Yes, I have a resume. You can find it on my website: https://www.bensultan.com"]
+        responses: ["Yes, I have a resume. You can find it on my website: https://www.bensultan.com."]
     },
 
     "projects" : {
         tags: ['projects', 'personal', 'coding', 'professional'],
-        responses: ["Yes, I have many coding projects that you can view at https://www.bensultan.com"]
+        responses: ["Yes, I have many coding projects that you can view at https://www.bensultan.com."]
     },
 
     "hobbies" : {
         tags: ['what', 'have', 'hobbies', 'free', 'time', 'interests', 'activities', 'talents', 'talent', 'special', 'do', 'other', 'personal', 'fun'],
-        responses: ["I enjoy writing and recording music, and I am a card-carrying member of the International Jugglers' Association"]
+        responses: ["I enjoy writing and recording music, and I am a card-carrying member of the International Jugglers' Association."]
     },
 
     "strengths" : {
@@ -74,27 +74,27 @@ _phraseList = {
     },
 
     "weaknesses" : {
-        tags: ['what', 'weakness', 'weaknesses', 'shortcoming', 'shortcomings'],responses: ["Chocolate and peanut butter."]
+        tags: ['what', 'weakness', 'weaknesses', 'shortcoming', 'shortcomings'],responses: ["The combination of chocolate and peanut butter."]
     },
 
     "questions" : {
         tags: ['questions', 'me', 'for', 'ask'],
-        responses: ["No, but since you stayed this long to chat, I think you should reach out to the real Ben. Something tells me that he'll be very interested in speaking with you."]
+        responses: ["I think you asked if I have any questions. No I don't, but since you seem interested in my candidacy, you should reach out to the real Ben. He will be interested in speaking with you."]
     },
 
     "education" : {
         tags: ['degree', 'education', 'highest', 'school', 'graduate', 'science', 'school', 'college', 'where'],
-        responses: ["My highest degree is a master's in education from Sarah Lawrence College. I also have a web development certificate from the CodeCademy Mentorship Program. Ben would love to give you more details about his unique mentorship-based training in a phone interview."]
+        responses: ["My highest degree is a master's in education from Sarah Lawrence College. I am a 'self-taught' engineer in the sense that I was trained by a variety of engineering mentors."]
     },
 
     "stack" : {
         tags: ['full-stack', 'full', 'stack', 'front', 'back', 'framework', 'frameworks'],
-        responses: ["I am a full stack developer. For my personal projects, I normally use something close to the MERN stack, with a couple of modifications, such as replacing MongoDB with Firebase, my prefered NoSQL database."]
+        responses: ["I am a full stack developer. My personal projects tend to be built with the MERN stack (although with Firebase NoSQL instead of MongoDB)."]
     },
 
     "years" : {
         tags: ['how', 'years', 'experience', 'professional', 'do', 'programming', 'job', 'work', 'working', 'many'],
-        responses: ["I have been developing personal projects for two years. I have 8 months' professional experience as a software engineer."]
+        responses: ["I have been developing personal projects for three years. I have two years' professional experience as a software engineer."]
     },
     "greeting" : {
         tags: ['hello', 'hey', 'hi', 'greetings', 'hola', 'howdy'],
@@ -106,7 +106,7 @@ _phraseList = {
     },
     "contact" : {
         tags: ["contact, info, email, phone, call"],
-        responses: ["It sounds like I really nailed this virtual interview. The real Ben's email is bensultan1985@gmail.com. Ben is going to be excited to hear from you! And I might get a bonus for my good work. Right now, I live as a sub-domain on bensultan.com, but I'm saving up for my very own domain.", "The real Ben's email address is bensultan1985@gmail.com. He is going to be so happy to hear from you."]
+        responses: ["It sounds like I really nailed this virtual interview. The real Ben's email is bensultan1985@gmail.com. Ben is going to be excited to hear from you! And I might get a bonus for my good work. Right now, I live as a sub-domain on bensultan.com, but I'm saving up for my very own domain.", "The real Ben's email address is bensultan1985@gmail.com. He will be happy to hear from you."]
     },
     "schedule" : {
         tags: ["appointment", "schedule", "interview", "real"],
@@ -114,11 +114,11 @@ _phraseList = {
     },
     "interpersonal" : {
         tags: ["style", "communication", "colleagues", "along", "conflict", "issues", "issue", "approach", "other"],
-        responses: ["I think you are asking me about how I communicate with people at work. I am professional and communicate clearly and effectively. If interpersonal conflicts arise, I will address the problem directly and immediately so that we can return to a friendly, productive environment."]
+        responses: ["I think you are asking me about my soft skills. I am professional and communicate clearly and effectively. If interpersonal conflict were to arise, I would address the problem directly, so that we can return to a friendly, productive environment."]
     },
     "solving": {
         tags: ["problem", "problems", "challenge", "approach", "issue", "issues", "bugs", "bug", "debugging", "deadlines", "deadline", "requirements"],
-        responses: ["I design and plan from the top down before beginning to code, which helps me avoid major issues later. However, bugs and challenges arise often in programming. I enjoy solving problems."]
+        responses: ["I design and plan with a top-down approach, which helps me avoid major issues later. However, bugs and challenges are a part of programming, and I enjoy solving those problems."]
     },
     "company" : {
         tags: ["what", "kind", "type", "company", "dev", "team", "development", "products", "software", "like", "want", "environment"],
@@ -126,11 +126,11 @@ _phraseList = {
     },
     "position" : {
         tags : ["developer", "programmer", "job", "position", "role", "level", "what", "looking", "want", "engineer", "seeking"],
-        responses: ["I am seeking a position as a Software Engineer."]
+        responses: ["I am seeking a position as a mid-level Software Engineer."]
     },
     "languages" : {
         tags : ['languages', 'language', 'favorite', 'programming', 'how', 'many', 'do', 'know', 'can', 'which', 'what'],
-        responses: ['My strongest language is Javascript. I also use HTML5/CSS3, Node.js, Express.js, React.js, C++, SQL, and some others.']
+        responses: ['My strongest language is Javascript. I also use HTML5/CSS3, C++, SQL, and some others.']
     },
     "memory-problem" : {
         tags : ['descibe', 'time', 'when', 'problem', 'issue', 'difficult', 'deadline'],
@@ -142,19 +142,19 @@ _phraseList = {
     },
     "memory-traits" : {
         tags : ['describe', 'tell', 'traits', 'trait', 'qualities', 'personality', 'three', 'leadership'],
-        responses: ['I would describe myself as thoughtful, logic, and creative.']
+        responses: ['I would describe myself as thoughtful, logical, and creative.']
     },
     "presenting" : {
         tags : ['present', 'audience', 'group', 'teach', 'presentation', 'meetings', 'clearly', 'represent', 'comfortable'],
-        responses: ['I think you asked if I am comfortable with public speaking. Yes, I am enjoy giving presentations and sharing ideas.']
+        responses: ['I think you asked if I am comfortable with public speaking. Yes, I enjoy giving presentations and sharing ideas.']
     },
     "whyhire" : {
         tags: ['why', 'hire', 'should'],
-        responses : ['I have a track record of delivering quality software on time, I am an effective communicator, and I am a great problem solver.']
+        responses : ['I have a track record of collaborating well with others and delivering quality software on time.']
     },
     "http" : {
         tags: ['describe', 'http', 'request', 'can', 'detail', 'details', 'explain', 'happens'],
-        responses: ['Ask the real Ben for all the details, but an HTTP request works as follows: When a user enters a web address, a call is made to the DNS server. The DNS server then connects the client to the requested server. Then, the client sends the HTTP request to the server. Finally, the server sends back a response.']
+        responses: ['I think you are asking me a technical quesiton about HTTP requests. An HTTP request works as follows: When a user enters a web address, a call is made to the DNS server. The DNS server then connects the client to the requested server. Then, the client sends the HTTP request to the server. Finally, the server sends back a response.']
     },
     "meaning" : {
         tags: ['meaning', 'life'],
